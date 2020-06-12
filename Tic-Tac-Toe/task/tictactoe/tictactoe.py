97f7c93f674a75f393a21c721fc5e714a24f6473
Aman Madan <madan.aman@gmail.com>
2020-06-12 22:45:41 +0530

    Stage 2... Now tic tac toe can display the current state based on its input.

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -5,27 +5,54 @@
 #     for x in i:
 #         row = row + x + " "
 #     print(row)
+# import numpy as np
+
 class Symbol:
     x = "X"
     o = "O"
     _ = " "
 
 
+class wrongStringInputException(Exception):
+    pass
+
+
 class TicTacToe:
     def __init__(self):
         self.feild = [
-            [Symbol.x, Symbol.o, Symbol.x],
-            [Symbol.o, Symbol.x, Symbol.o],
-            [Symbol.x, Symbol.x, Symbol.o]
+            [Symbol._, Symbol._, Symbol._],
+            [Symbol._, Symbol._, Symbol._],
+            [Symbol._, Symbol._, Symbol._]
         ]
+        self.rows = 3
+        self.columns = 3
+
+    def set_state(self, str):
+        if len(str) == 9:
+            for i in range(0, self.rows):
+                for j in range(0, self.columns):
+                    self.feild[i][j] = str[(j % 3) + (i * 3)]
+        else:
+            raise wrongStringInputException
+        # self.feild = np.array(str)
 
     def print_state(self):
-        for i in self.feild:
+        print("---------")
+        for i in range(0, self.rows):
             row = ""
-            for x in i:
-                row = row + x + " "
+            for x in range(0, self.columns):
+                if x == 0:
+                    row = row + "| "
+                row = row + self.feild[i][x] + " "
+                if x == self.columns - 1:
+                    row = row + "|"
             print(row)
+        print("---------")
 
 
 game = TicTacToe()
+try:
+    game.set_state("O_OXXO_XX")
+except wrongStringInputException:
+    print("Wrong string passed")
 game.print_state()
