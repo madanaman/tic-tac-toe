08e075f553deb31707d23ebface3a629d5761180
Aman Madan <madan.aman@gmail.com>
2020-06-14 16:55:14 +0530

    Stage 4 completed. This validates user input

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -6,6 +6,8 @@
 #         row = row + x + " "
 #     print(row)
 # import numpy as np
+import re
+
 
 class Symbol:
     x = "X"
@@ -15,6 +17,21 @@ class Symbol:
 
 class wrongStringInputException(Exception):
     pass
+
+
+class OutOfBoundCoordinatesException(Exception):
+    def __init__(self):
+        print("Coordinates should be from 1 to 3!")
+
+
+class NonIntCoordinatesException(Exception):
+    def __init__(self):
+        print("You should enter numbers!")
+
+
+class CellAlreadyFilledException(Exception):
+    def __init__(self):
+        print("This cell is occupied! Choose another one!")
 
 
 class TicTacToe:
@@ -40,21 +57,23 @@ class TicTacToe:
 
     def find_result(self):
         winning_dict = {
-            "r1": "0,1,2",
-            "r2": "3,4,5",
-            "r3": "6,7,8",
-            "c1": "0,3,6",
-            "c2": "1,4,7",
-            "c3": "2,5,8",
-            "d1": "0,4,8",
-            "d2": "2,4,6"
+            "r1": "0,0:0,1:0,2",
+            "r2": "1,0:1,1:1,2",
+            "r3": "2,0:2,1:2,2",
+            "c1": "0,0:1,0:2,0",
+            "c2": "0,1:1,1:2,1",
+            "c3": "0,2:1,2:2,2",
+            "d1": "0,0:1,1:2,2",
+            "d2": "2,0:2,2:0,2"
         }
         winner_x = False
         winner_o = False
 
         for i in winning_dict.keys():
-            cnt_x = [self.raw_string[int(pos)] for pos in winning_dict[i].split(",")].count("X")
-            cnt_o = [self.raw_string[int(pos)] for pos in winning_dict[i].split(",")].count("O")
+            cnt_x = [self.feild[int(x.split(",")[0])][int(x.split(",")[1])] for x in winning_dict[i].split(":")].count(
+                "X")
+            cnt_o = [self.feild[int(x.split(",")[0])][int(x.split(",")[1])] for x in winning_dict[i].split(":")].count(
+                "O")
             if cnt_x == 3:
                 winner_x = True
             elif cnt_o == 3:
@@ -88,11 +107,54 @@ class TicTacToe:
             print(row)
         print("---------")
 
+    def validate_n_set_coordinates(self, coordinates, symbol):
+        # check for numbers
+        # print("starting function")
+        x = coordinates.split(" ")
+        # print(x, y)
+        if re.match(r"[0-9]+", x[0]) is None or re.match(r"[0-9]", x[1]) is None:
+            raise NonIntCoordinatesException
+            # return False
+        if not 1 <= int(x[0]) <= 3 or not 1 <= int(x[1]) <= 3:
+            raise OutOfBoundCoordinatesException
+            # return False
+        # return True
+        coordinate_dict = {
+            "1 3": "0,0",
+            "2 3": "0,1",
+            "3 3": "0,2",
+            "1 2": "1,0",
+            "2 2": "1,1",
+            "3 2": "1,2",
+            "1 1": "2,0",
+            "2 1": "2,1",
+            "3 1": "2,2"
+        }
+        x, y = coordinate_dict[coordinates].split(",")
+        # print(self.feild[int(x)][int(y)])
+        if self.feild[int(x)][int(y)] != "_":
+            raise CellAlreadyFilledException
+        else:
+            self.feild[int(x)][int(y)] = symbol
 
+player_symbol = "X"
 game = TicTacToe()
 try:
     game.set_state(input())
 except wrongStringInputException:
     print("Wrong string passed")
 game.print_state()
-print(game.find_result())
+# print(game.find_result())
+restart_loop = True
+while restart_loop is True:
+    coordinates = input("Enter the coordinates: ")
+    try:
+        game.validate_n_set_coordinates(coordinates, player_symbol)
+    except OutOfBoundCoordinatesException:
+        continue
+    except NonIntCoordinatesException:
+        continue
+    except CellAlreadyFilledException:
+        continue
+    restart_loop = False
+game.print_state()
